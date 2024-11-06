6d87acc5d7ea8d864d4fcfa1d2ea85ef361a3ac7
Barrett Ray <66580279+onkoe@users.noreply.github.com>
2024-11-05 21:51:27 -0600

    feat(edid): make edidversion fields pub

--- edid/src/structures/version.rs
+++ edid/src/structures/version.rs
@@ -4,7 +4,7 @@
 #[derive(Clone, Debug, PartialEq, PartialOrd)]
 pub struct EdidVersion {
     /// Should be 0x01 for v1.4.
-    version: u8,
+    pub version: u8,
     /// Should be 0x04 for v1.4.
-    revision: u8,
+    pub revision: u8,
 }
