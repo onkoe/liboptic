fa5f50f0b5deb150a78e9d0158e97b4baaaf1575
barrett <onkoe@protonmail.com>
2024-11-05 18:45:18 -0600

    feat(edid/parser): check length first

--- edid/src/parser/mod.rs
+++ edid/src/parser/mod.rs
@@ -1,11 +1,32 @@
-use winnow::PResult;
+use winnow::{
+    error::{ErrMode, ParserError as _},
+    PResult,
+};
 
 mod header;
 pub(super) mod util;
 
 pub fn parse(input: &mut &[u8]) -> PResult<super::Edid> {
+    // check the length
+    check_length(input)?;
+
     // make sure header's right
     header::parse(input)?;
 
     todo!()
+}
+
+fn check_length(input: &&[u8]) -> PResult<()> {
+    let expected_len = 0x7F;
+    let real_len = input.len();
+
+    if real_len < expected_len {
+        tracing::error!("The length is too short: (got: `{real_len}`, expected: `{expected_len}`)");
+        return Err(ErrMode::from_error_kind(
+            input,
+            winnow::error::ErrorKind::Verify,
+        ));
+    }
+
+    Ok(())
 }
