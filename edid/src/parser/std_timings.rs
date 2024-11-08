6875bdf5f23ce8d6eecb23ecb7836a7d1f9f63c8
barrett <onkoe@protonmail.com>
2024-11-08 00:08:27 -0600

    feat(edid/std_timings): make `STiming`s optional
    i've looked at a lotta real-world samples and seems like vendors often leave them at [0x01, 0x01] when unused. so i'll count that as the None value.

--- edid/src/parser/std_timings.rs
+++ edid/src/parser/std_timings.rs
@@ -3,7 +3,7 @@ use bitvec::{field::BitField, view::BitView};
 use crate::prelude::internal::*;
 
 /// Finds the standard timings for this display.
-#[tracing::instrument]
+#[tracing::instrument(skip_all)]
 pub(crate) fn parse(input: &[u8]) -> StandardTimings {
     // yeah im not using unsafe to avoid the array here. hardcoding it is :D
     StandardTimings {
@@ -21,29 +21,38 @@ pub(crate) fn parse(input: &[u8]) -> StandardTimings {
 /// Checks the info of the given standard timing.
 ///
 /// Make sure the given bytes are correctly aligned.
+///
+/// While these aren't noted to be optional in the standard, vendors tend to
+/// treat them as if they were.
 #[tracing::instrument]
-fn one(bytes: &[u8]) -> STiming {
-    // from [8, 2288] px
+fn one(bytes: &[u8]) -> Option<STiming> {
+    // if both bytes are 1, assume unused
+    if bytes[0] == 0x01 && bytes[1] == 0x01 {
+        return None;
+    }
+
+    // from [256, 2288] px
     let horizontal_addr_pixel_ct = hoz_addr_pixels(bytes[0]);
 
     // split the other byte into bits
     let bits: &bitvec::prelude::BitSlice<u8> = bytes[1].view_bits();
 
     // find aspect ratio
+    tracing::debug!("aspect ratio: {:#?}", (bits[7], bits[6]));
     let aspect_ratio = match (bits[7], bits[6]) {
-        (false, false) => std_timings::StandardAspectRatio::_16_10,
-        (false, true) => std_timings::StandardAspectRatio::_4_3,
-        (true, false) => std_timings::StandardAspectRatio::_5_4,
-        (true, true) => std_timings::StandardAspectRatio::_16_9,
+        (false, false) => StandardAspectRatio::_16_10,
+        (false, true) => StandardAspectRatio::_4_3,
+        (true, false) => StandardAspectRatio::_5_4,
+        (true, true) => StandardAspectRatio::_16_9,
     };
 
     let field_refresh_rate = bits[0..=5].load_be::<u8>() + 60;
 
-    STiming {
+    Some(STiming {
         horizontal_addr_pixel_ct,
         aspect_ratio,
         field_refresh_rate,
-    }
+    })
 }
 
 /// Finds the number of horizontal addressable pixels from the given value.
