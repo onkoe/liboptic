e219d73c02e0605feb9cf47825c1c4eda8bbb456
barrett <onkoe@protonmail.com>
2024-11-07 18:18:28 -0600

    feat(edid/util): warn on wrong file in tests

--- edid/src/parser/util.rs
+++ edid/src/parser/util.rs
@@ -14,6 +14,13 @@ pub(crate) fn edid_by_filename(name: &str) -> alloc::vec::Vec<u8> {
         );
     }
 
+    if name.contains(".raw") {
+        tracing::warn!(
+            "This might be the wrong test file. (should be hex, but contains\
+            `.raw`)"
+        );
+    }
+
     let path =
         std::path::PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR"), "/tests/assets")).join(name);
     let s = std::fs::read_to_string(path)
@@ -26,6 +33,20 @@ pub(crate) fn edid_by_filename(name: &str) -> alloc::vec::Vec<u8> {
 #[cfg(test)]
 #[tracing::instrument]
 pub(crate) fn raw_edid_by_filename(name: &str) -> alloc::vec::Vec<u8> {
+    if name.contains(".info") {
+        tracing::warn!(
+            "You're probably passing in a `.info` file for the test.\
+        However, you probably meant to use a `.input` file instead!"
+        );
+    }
+
+    if !name.contains(".raw") {
+        tracing::warn!(
+            "You might accidentally be passing in an incorrect test file. Should\
+            contain `.raw` but doesn't."
+        );
+    }
+
     let path =
         std::path::PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR"), "/tests/assets")).join(name);
 
