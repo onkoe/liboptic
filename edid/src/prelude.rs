940cc9d099b5e751cd3fe8babf5e895fe8987b9c
barrett <onkoe@protonmail.com>
2024-11-07 19:51:49 -0600

    feat(edid): add some stuff to the prelude

--- edid/src/prelude.rs
+++ edid/src/prelude.rs
@@ -1,4 +1,25 @@
 pub(crate) mod internal {
     #[cfg(test)]
     pub(crate) use crate::parser::util::{edid_by_filename, logger, raw_edid_by_filename};
+
+    // probably the most important part lol
+    pub(crate) use crate::Edid;
+
+    // structure modules
+    pub(crate) use crate::structures::{
+        _18bytes, basic_info, color, est_timings, extension, id, std_timings, version,
+    };
+
+    pub(crate) use crate::structures::basic_info::{
+        feature_support::{
+            ColorEncodingFormats, ColorSupport, ColorType, FeatureSupport, PowerManagement,
+        },
+        vsi::{
+            analog,
+            digital::{ColorBitDepth, SupportedVideoInterface},
+            VideoSignalInterface,
+        },
+        BasicDisplayInfo, SizeOrRatio,
+    };
+
 }
