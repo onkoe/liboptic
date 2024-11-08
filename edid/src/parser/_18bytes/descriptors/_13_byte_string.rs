c515b1ef68b460113d7c9872f6f32dbc6b5d2307
barrett <onkoe@protonmail.com>
2024-11-08 12:30:32 -0600

    test(edid/desc): add sample tests from edid std

--- edid/src/parser/_18bytes/descriptors/_13_byte_string.rs
+++ edid/src/parser/_18bytes/descriptors/_13_byte_string.rs
@@ -32,3 +32,43 @@ pub(crate) fn parse(input: &[u8; 18]) -> ArrayString<13> {
 
     string
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::prelude::internal::*;
+
+    #[test]
+    fn std_sample_thisisatest_string() {
+        logger();
+        // note: first five in this array are for a data string descriptor
+        let bytes = [
+            0x00, 0x00, 0x00, 0xFE, 0x00, 0x54, 0x48, 0x49, 0x53, 0x49, 0x53, 0x41, 0x54, 0x45,
+            0x53, 0x54, 0x0a, 0x20,
+        ];
+        let got = parse(&bytes);
+        tracing::info!("got: {got}");
+
+        let expected = ArrayString::from("THISISATEST").unwrap();
+        tracing::warn!("expected: {expected}");
+
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn std_sample_a0123456789_string() {
+        logger();
+        // note: from the serial number descriptor example
+        let bytes = [
+            0x00, 0x00, 0x00, 0xFF, 0x00, 0x41, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37,
+            0x38, 0x39, 0x0a, 0x20,
+        ];
+        let got = parse(&bytes);
+        tracing::info!("got: {got}");
+
+        let expected = ArrayString::from("A0123456789").unwrap();
+        tracing::warn!("expected: {expected}");
+
+        assert_eq!(got, expected);
+    }
+}
