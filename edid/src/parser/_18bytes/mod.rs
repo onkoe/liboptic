2b4d1a6ff026a0b956315f8c953c3820e4cbb286
barrett <onkoe@protonmail.com>
2024-11-08 13:04:15 -0600

    feat(edid/parse/18b): impl desc kind detection

--- edid/src/parser/_18bytes/mod.rs
+++ edid/src/parser/_18bytes/mod.rs
@@ -1,4 +1,5 @@
-use crate::prelude::internal::*;
+use crate::{prelude::internal::*, structures::desc::DisplayDescriptor};
+use descriptors::*;
 
 mod descriptors;
 mod preferred_tm;
@@ -11,7 +12,52 @@ pub(crate) fn parse(input: &[u8]) -> Result<EighteenByteDescriptors, EdidError> 
     let fourth = &input[0x6C..=0x7D];
 
     // the first one is ALWAYS a preferred timing mode.
-    let first_parsed = preferred_tm::parse(first.try_into()?);
+    let preferred_timing_mode = preferred_tm::parse(first.try_into()?);
 
-    todo!()
+    let blocks = [
+        one(second.try_into()?)?,
+        one(third.try_into()?)?,
+        one(fourth.try_into()?)?,
+    ];
+
+    Ok(EighteenByteDescriptors {
+        preferred_timing_mode,
+        blocks,
+    })
+}
+
+/// Parses the given eighteen-byte block.
+#[tracing::instrument(skip_all)]
+fn one(input: &[u8; 18]) -> Result<EighteenByteBlock, EdidError> {
+    // if the first two bytes aren't both zero, it's a timing definition
+    if input[0] != 0x00 && input[1] != 0x00 {
+        return Ok(EighteenByteBlock::Timing(preferred_tm::parse(input)));
+    }
+
+    // otherwise, we're making a display descriptor.
+    //
+    // let's make sure it has the required reserved byte.
+    {
+        let reserved = input[2];
+        if reserved != 0x00 {
+            tracing::error!("EDID contained ambiguous descriptor! Malformed byte: `{reserved}`");
+            return Err(EdidError::AmbiguousDescriptor {
+                malformed_byte: reserved,
+            });
+        }
+    }
+
+    // okay! we know that it's some kind of descriptor.
+    //
+    // the specific kind we're making is indicated at byte 3. let's see what
+    // that is and call the appropriate parser
+    let kind_byte = input[3];
+    let desc = match kind_byte {
+        0xFF => DisplayDescriptor::ProductSerial(_13_byte_string::parse(input)),
+        0xFE => DisplayDescriptor::DataString(_13_byte_string::parse(input)),
+        0x11..=0xF6 => return Err(EdidError::DescriptorUsedReservedKind { kind_byte }),
+        ty => todo!("this descriptor type (`{ty:x}`) is unimplemented!"),
+    };
+
+    Ok(EighteenByteBlock::Display(desc))
 }
