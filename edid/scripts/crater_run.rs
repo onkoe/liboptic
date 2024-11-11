f49eed7aaaa0b066aa63fa91be2517a94b39b11d
Barrett Ray <66580279+onkoe@users.noreply.github.com>
2024-11-11 14:22:54 -0600

    refactor(edid/scripts/crater): expect fails
    some edids really should fail, but most should be accepted for this parser to be useful lol

--- edid/scripts/crater_run.rs
+++ edid/scripts/crater_run.rs
@@ -30,6 +30,8 @@ use liboptic_edid::{error::EdidError, Edid};
 
 static mut PANICKED: u32 = 0_u32;
 
+static mut SKIPPED: u32 = 0_u32;
+
 const PATH: &str = "/home/barrett/Downloads/linuxhw_edid_repo";
 const LIST_ERRS: bool = false;
 
@@ -54,15 +56,17 @@ fn main() -> anyhow::Result<()> {
         " passed".green()
     );
 
-    let failed = format!(
-        "{}{}",
+    let expected_fails = format!(
+        "{}{}{}",
         parsed_edids
             .iter()
-            .filter(|pe| pe.parsed_edid.is_err())
+            .filter(|pe| pe.parsed_edid.is_err() && matches!(pe.should_fail, ExpectedFailure::Yes))
             .count()
             .to_string()
-            .bright_red(),
-        " failed".red()
+            .bright_red()
+            .on_black(),
+        " failed... ".red().on_black(),
+        "expectedly".green().on_black()
     );
 
     let panicked = format!(
@@ -74,14 +78,14 @@ fn main() -> anyhow::Result<()> {
     // afterwards, we can list the results
     if LIST_ERRS {
         for edid in parsed_edids {
-            if let Err(e) = edid.parsed_edid {
+            if let Err(ref e) = edid.parsed_edid {
                 println!("{} {}\n", "[ERR]".black().on_red(), e.to_string().red());
             }
         }
     }
 
     // and print the strings
-    println!("({passed}/{failed}/{panicked})");
+    println!("({passed}/{expected_fails}/{panicked})");
 
     Ok(())
 }
@@ -93,26 +97,36 @@ fn parse_all() -> Vec<Entry> {
 
     // run on each entry in both digital and analog
     let timer = Instant::now();
-    let paths = [path.join("Digital"), path.join("Analog")]
-        .par_iter()
-        .flat_map(|p| {
-            let inner: Vec<_> = jwalk::WalkDir::new(p)
-                .into_iter()
-                .flat_map(|entry| {
-                    if let Ok(entry) = entry {
-                        Some(entry.path())
-                    } else {
-                        None
+    let dirs = [path.join("Digital"), path.join("Analog")];
+
+    let mut runs = Vec::<Entry>::new();
+    for dir in dirs {
+        for entry in walkdir::WalkDir::new(dir) {
+            if let Ok(entry) = entry {
+                if let Some(res) = run(entry.path()) {
+                    runs.push(res.clone());
+                    if res.parsed_edid.is_err() {
+                        println!(
+                            "err on {}th run! (path: {})",
+                            runs.len(),
+                            res.path.display()
+                        );
+                        hex(&res.raw_edid);
                     }
-                })
-                .map(run) // run the logic
-                .collect();
-            inner
-        })
-        .collect::<Vec<Option<_>>>()
-        .into_iter()
-        .flatten()
-        .collect::<Vec<_>>();
+
+                    if matches!(res.should_fail, ExpectedFailure::Yes) {
+                        // expected to fail. all good!
+                        assert!(
+                            res.parsed_edid.is_err(),
+                            "expected fail should fail, but was ok: {res:#?}"
+                        );
+                    } else {
+                        res.parsed_edid.expect("found an unexpected failure.");
+                    }
+                }
+            }
+        }
+    }
 
     // say how long it took
     println!(
@@ -125,38 +139,62 @@ fn parse_all() -> Vec<Entry> {
         .on_green()
     );
 
-    paths
+    runs
 }
 
 /// An EDID we parsed.
+#[derive(Clone, Debug)]
 struct Entry {
     parsed_edid: Result<Edid, EdidError>,
     raw_edid: Vec<u8>,
     path: PathBuf,
+    should_fail: ExpectedFailure,
+}
+
+/// prints a string of hex bytes, causing a given byte array to be more
+/// readable.
+fn hex(input: &[u8]) {
+    // print column numbers
+    print!("{}", "[--]: ".blue());
+    for c in 0x00..0x10 {
+        print!("{}", format!("{c:02x} ").blue())
+    }
+    println!();
+
+    for (chunk_num, chunk) in input.chunks(16).enumerate() {
+        // print the chunk number before the actual data
+        print!("{}", &format!("[{chunk_num:02x}]: ").red());
+
+        for byte in chunk {
+            print!("{}", format!("{:02x} ", byte).as_str());
+        }
+        println!();
+    }
 }
 
 /// contains logic to run on both digital and analog displays.
 fn run<P: AsRef<Path>>(entry_path: P) -> Option<Entry> {
     let entry_path = entry_path.as_ref();
 
-    if let Some(edid) = edid_by_filename(entry_path) {
-        return Some(Entry {
-            parsed_edid: Edid::new(edid.clone()),
-            raw_edid: edid,
-            path: entry_path.into(),
-        });
+    if let Some((bytes, should_fail)) = edid_by_filename(entry_path) {
+        // return Some(Entry {
+        //     parsed_edid: Edid::new(edid.clone()),
+        //     raw_edid: bytes,
+        //     path: entry_path.into(),
+        //     should_fail,
+        // });
 
-        /*
         // parse it and return the results
-        let parse_result = std::panic::catch_unwind(|| Edid::new(edid.clone()));
+        let parse_result = std::panic::catch_unwind(|| Edid::new(bytes.clone()));
 
         // report any panic
         match parse_result {
             Ok(parsed_edid) => {
                 return Some(Entry {
                     parsed_edid,
-                    raw_edid: edid,
+                    raw_edid: bytes,
                     path: entry_path.into(),
+                    should_fail,
                 });
             }
             Err(_) => {
@@ -168,7 +206,6 @@ fn run<P: AsRef<Path>>(entry_path: P) -> Option<Entry> {
                 return None;
             }
         }
-        */
     }
 
     None
@@ -176,9 +213,27 @@ fn run<P: AsRef<Path>>(entry_path: P) -> Option<Entry> {
 
 /// Grabs an EDID from disk at the given path.
 #[tracing::instrument]
-pub(crate) fn edid_by_filename(path: &Path) -> Option<Vec<u8>> {
+pub(crate) fn edid_by_filename(path: &Path) -> Option<(Vec<u8>, ExpectedFailure)> {
     let path = PathBuf::from(path);
     let s = read_to_string(&path).ok()?;
+
+    let mut expected_failure = ExpectedFailure::No;
+
+    // skip any that we don't expect to work
+    {
+        if s.contains("Vendor & Product Identification: Manufacturer name field contains garbage.")
+        {
+            expected_failure = ExpectedFailure::Yes;
+        }
+
+        if s.contains("Display Range Limits: Byte 10 is ") && s.contains(" instead of 0x0a.") {
+            expected_failure = ExpectedFailure::Yes;
+        }
+
+        if s.contains("Display Range Limits: Unknown range class") {
+            expected_failure = ExpectedFailure::Yes;
+        }
+    }
 
     // skip any non-edid files
     if !s.contains("edid-decode (hex):") {
@@ -200,5 +255,11 @@ pub(crate) fn edid_by_filename(path: &Path) -> Option<Vec<u8>> {
         eprintln!("{s}")
     });
 
-    hr.ok()
+    hr.ok().map(|hr| (hr, expected_failure))
+}
+
+#[derive(Clone, Debug)]
+enum ExpectedFailure {
+    Yes,
+    No,
 }
